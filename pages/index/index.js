ea9a288f510ef66fa8c98668fe2561dd42d12b5c
qqack <1262197007@qq.com>
2017-08-13 21:28:23 +0800

    使用scroll-view组件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,38 @@
-//index.js
-//获取应用实例
+var order = ['red', 'yellow', 'blue', 'green', 'red']
 var app = getApp()
 Page({
   data: {
+    toView: 'red',
+    scrollTop: 100,
     motto: '欢迎进入懒球记账！',
     userInfo: {}
   },
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -16,10 +41,10 @@ Page({
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
   }
